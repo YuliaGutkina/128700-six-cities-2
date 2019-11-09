ec0436297f180a160920e4a72c332a51eb4a649e
Julia Gutkina <julia.gutkina@perfectart.com>
2019-11-09 19:47:46 +0200

    Update test to City Map

--- src/components/city-map/city-map.test.js
+++ src/components/city-map/city-map.test.js
@@ -3,12 +3,22 @@ import renderer from "react-test-renderer";
 import {CityMap} from "./city-map";
 import {offers} from "../../mocks/offers";
 
+function createNodeMock(element) {
+  if (element.type === `section`) {
+    return {
+      focus() {},
+    };
+  }
+  return null;
+}
+
 it(`Map correctly renders after relaunch`, () => {
+  const options = {createNodeMock};
   const tree = renderer
     .create(<CityMap
       items={offers}
       initialCity={[52.38333, 4.9]}
-    />)
+    />, options)
     .toJSON();
 
   expect(tree).toMatchSnapshot();
