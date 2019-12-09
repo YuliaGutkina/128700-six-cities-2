e10f46ce9cbd7daf818827828949990ec15f90d2
Julia Gutkina <julia.gutkina@perfectart.com>
2019-12-09 08:57:24 +0200

    Add empty main page

--- src/components/main-page/main-page.jsx
+++ src/components/main-page/main-page.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from "prop-types";
 import {connect} from "react-redux";
+import classNames from "classnames";
 
 import {OffersList} from "../offers-list/offers-list";
 import {CityMap} from "../city-map/city-map";
@@ -18,25 +19,28 @@ const MainPage = (props) => {
 
   return <div className="page page--gray page--main">
     <Header/>
-    <main className="page__main page__main--index">
+    <main className={classNames(`page__main page__main--index`, {
+      "page__main--index-empty": !cityOffers.length
+    })}>
       <h1 className="visually-hidden">Cities</h1>
       <Locations
         currentCity={cityName}
       />
       <div className="cities">
-        <div className="cities__places-container container">
-          <section className="cities__places places">
-            <h2 className="visually-hidden">Places</h2>
-            <b className="places__found">{cityOffers.length} places to stay in {cityName}</b>
-            <form className="places__sorting" action="#" method="get">
-              <span className="places__sorting-caption">Sort by</span>
-              <span className="places__sorting-type" tabIndex={0}>
+        {cityOffers.length ?
+          <div className="cities__places-container container">
+            <section className="cities__places places">
+              <h2 className="visually-hidden">Places</h2>
+              <b className="places__found">{cityOffers.length} places to stay in {cityName}</b>
+              <form className="places__sorting" action="#" method="get">
+                <span className="places__sorting-caption">Sort by</span>
+                <span className="places__sorting-type" tabIndex={0}>
                     Popular
-                <svg className="places__sorting-arrow" width={7} height={4}>
-                  <use xlinkHref="#icon-arrow-select" />
-                </svg>
-              </span>
-              { /*
+                  <svg className="places__sorting-arrow" width={7} height={4}>
+                    <use xlinkHref="#icon-arrow-select"/>
+                  </svg>
+                </span>
+                {/*
             <ul className="places__options places__options--custom places__options--opened">
               <li className="places__option places__option--active" tabIndex={0}>Popular</li>
               <li className="places__option" tabIndex={0}>Price: low to high</li>
@@ -44,7 +48,7 @@ const MainPage = (props) => {
               <li className="places__option" tabIndex={0}>Top rated first</li>
             </ul>
             */}
-              {/*
+                {/*
                 <select class="places__sorting-type" id="places-sorting">
                   <option class="places__option" value="popular" selected="">Popular</option>
                   <option class="places__option" value="to-high">Price: low to high</option>
@@ -52,24 +56,34 @@ const MainPage = (props) => {
                   <option class="places__option" value="top-rated">Top rated first</option>
                 </select>
                 */}
-            </form>
-            <div className="cities__places-list places__list tabs__content">
-              <OffersListWrapped
-                places={cityOffers}
-                cardClassName="cities__place-card"
-                imageWrapperClassName="cities__image-wrapper"
-                imageWidth={260}
-                imageHeight={200}
+              </form>
+              <div className="cities__places-list places__list tabs__content">
+                <OffersListWrapped
+                  places={cityOffers}
+                  cardClassName="cities__place-card"
+                  imageWrapperClassName="cities__image-wrapper"
+                  imageWidth={260}
+                  imageHeight={200}
+                />
+              </div>
+            </section>
+            <div className="cities__right-section">
+              <CityMap
+                items={cityOffers}
+                currentCity={city}
               />
             </div>
-          </section>
-          <div className="cities__right-section">
-            <CityMap
-              items={cityOffers}
-              currentCity={city}
-            />
+          </div> :
+          <div className="cities__places-container cities__places-container--empty container">
+            <section className="cities__no-places">
+              <div className="cities__status-wrapper tabs__content">
+                <b className="cities__status">No places to stay available</b>
+                <p className="cities__status-description">We could not find any property availbale at the moment in {cityName}</p>
+              </div>
+            </section>
+            <div className="cities__right-section" />
           </div>
-        </div>
+        }
       </div>
     </main>
   </div>;
