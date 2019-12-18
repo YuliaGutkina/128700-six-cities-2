c236384b50b623d54e3b9fbeb9873175eef23f11
Julia Gutkina <julia.gutkina@perfectart.com>
2019-12-18 09:56:13 +0200

    Add routing to locations

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,6 +1,6 @@
 import React, {PureComponent} from 'react';
 import PropTypes from "prop-types";
-import {Switch, Route} from "react-router-dom";
+import {Switch, Route, Redirect} from "react-router-dom";
 import {connect} from "react-redux";
 
 import MainPage from "../main-page/main-page";
@@ -11,15 +11,23 @@ import Offer from "../offer/offer";
 import withAuth from "../../hocs/with-auth/with-auth";
 import withoutAuth from "../../hocs/without-auth/without-auth";
 import {Operation} from "../../reducer/user/user";
+import {receiveCityInfoSelector} from "../../reducer/data/selectors";
+import {ComplexPropType} from "../../types/types";
 
 
 const LoginWrapped = withLoginFormSubmit(Login);
 
 class App extends PureComponent {
   render() {
+    const {city} = this.props;
+    const cityName = city ? city.name : ``;
+
     return (
       <Switch>
-        <Route path="/" exact component={MainPage}/>
+        <Route path="/" exact>
+          <Redirect to={`/${cityName}`} />
+        </Route>
+        <Route path="/:city" exact component={MainPage}/>
         <Route path="/favorites" exact component={withAuth(Favorites)}/>
         <Route path="/login" exact component={withoutAuth(LoginWrapped)}/>
         <Route path="/offer/:id" exact component={Offer}/>
@@ -33,8 +41,13 @@ class App extends PureComponent {
 }
 
 App.propTypes = {
-  onAuthCheck: PropTypes.func
+  onAuthCheck: PropTypes.func,
+  city: ComplexPropType.CITY_INFO
 };
+
+const mapStateToProps = (state, ownProps) => Object.assign({}, ownProps, {
+  city: receiveCityInfoSelector(state)
+});
 
 const mapDispatchToProps = {
   onAuthCheck: Operation.echoUser
@@ -42,4 +55,4 @@ const mapDispatchToProps = {
 
 
 export {App};
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
